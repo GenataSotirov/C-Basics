9fd463bf3e6a9cb330904db5c6e437f284238d30
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:09:42 +0200

    Add files via upload

---
+++ 05. Vacation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _05._Vacation
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string house = string.Empty;
+            string location = string.Empty;
+            double price = 0;
+
+            if (budget <= 1000)
+            {
+                house = "Camp";
+                switch (season)
+                {
+                    case "Summer":
+                        price = budget * 0.65;
+                        location = "Alaska";
+                        break;
+                    case "Winter":
+                        price = budget * 0.45;
+                        location = "Morocco";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (budget <= 3000)
+            {
+                house = "Hut";
+                switch (season)
+                {
+                    case "Summer":
+                        price = budget * 0.8;
+                        location = "Alaska";
+                        break;
+                    case "Winter":
+                        price = budget * 0.6;
+                        location = "Morocco";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                house = "Hotel";
+                price = budget * 0.9;
+                if (season == "Summer")
+                {
+                    location = "Alaska";
+                }
+                else
+                {
+                    location = "Morocco";
+                }
+            }
+            Console.WriteLine($"{location} - {house} - {price:f2}");
+        }
+    }
+}
---
+++ 06. Truck Driver/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _06._Truck_Driver
+{
+    class Program
+    {
+        static void Main()
+        {
+            string season = Console.ReadLine();
+            double distance = double.Parse(Console.ReadLine());
+
+            double money = 0;
+
+            if (distance <= 5000)
+            {
+                if (season == "Summer")
+                {
+                    money = 4 * distance * 0.9;
+                }
+                else if (season == "Winter")
+                {
+                    money = 4 * distance * 1.05;
+                }
+                else
+                {
+                    money = 4 * distance * 0.75;
+                }
+            }
+            else if (distance <= 10000)
+            {
+                if (season == "Summer")
+                {
+                    money = 4 * distance * 1.1;
+                }
+                else if (season == "Winter")
+                {
+                    money = 4 * distance * 1.25;
+                }
+                else
+                {
+                    money = 4 * distance * 0.95;
+                }
+            }
+            else
+            {
+                money = 4 * distance * 1.45;
+            }
+            money = money - money * 0.1;
+            Console.WriteLine($"{money:f2}");
+        }
+    }
+}
