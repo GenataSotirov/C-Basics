ba4b3d89c751342d62133d9467275a990f50bdc8
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:36:18 +0200

    Add files via upload

---
+++ 10. Clock - part 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Clock___part_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 60; j++)
+                {
+                    for (int k = 0; k < 60; k++)
+                    {
+                        Console.WriteLine($"{i} : {j} : {k}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 11. Odd_Even Position/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _11._Odd_Even_Position
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double oddSum = 0;
+            double oddMin = double.MaxValue;
+            double oddMax = double.MinValue;
+            double evenSum = 0;
+            double evenMin = double.MaxValue;
+            double evenMax = double.MinValue;
+
+            if (n == 0)
+            {
+                Console.WriteLine($"OddSum={oddSum:f2},");
+                Console.WriteLine($"OddMin=No,");
+                Console.WriteLine($"OddMax=No,");
+                Console.WriteLine($"EvenSum={evenSum:f2},");
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+            else
+            {
+                bool more = false;
+                for (int i = 1; i <= n; i++)
+                {
+                    double num = double.Parse(Console.ReadLine());
+
+                    if (n==1)
+                    {
+                        Console.WriteLine($"OddSum={num:f2},");
+                        Console.WriteLine($"OddMin={num:f2},");
+                        Console.WriteLine($"OddMax={num:f2},");
+                        Console.WriteLine($"EvenSum={evenSum:f2},");
+                        Console.WriteLine($"EvenMin=No,");
+                        Console.WriteLine($"EvenMax=No");
+                        more = true;
+                    }
+                    else
+                    {
+                        if (i % 2 == 1)
+                        {
+                            oddSum += num;
+                            if (num < oddMin)
+                            {
+                                oddMin = num;
+                            }
+                            if (num > oddMax)
+                            {
+                                oddMax = num;
+                            }
+                        }
+                        else
+                        {
+                            evenSum += num;
+                            if (num < evenMin)
+                            {
+                                evenMin = num;
+                            }
+                            if (num > evenMax)
+                            {
+                                evenMax = num;
+                            }
+                        }
+                    }
+                }
+                if (!more)
+                {
+                    Console.WriteLine($"OddSum={oddSum:f2},");
+                    Console.WriteLine($"OddMin={oddMin:f2},");
+                    Console.WriteLine($"OddMax={oddMax:f2},");
+                    Console.WriteLine($"EvenSum={evenSum:f2},");
+                    Console.WriteLine($"EvenMin={evenMin:f2},");
+                    Console.WriteLine($"EvenMax={evenMax:f2}");
+                }
+            }
+        }
+    }
+}
