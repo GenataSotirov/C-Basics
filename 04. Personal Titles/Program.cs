9575613e898d4f6430966296dcff7cb5c1606805
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:03:34 +0200

    Add files via upload

---
+++ 04. Personal Titles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    class Program
+    {
+        static void Main()
+        {
+            double age = double.Parse(Console.ReadLine());
+            char pol = char.Parse(Console.ReadLine());
+
+            if (pol == 'm')
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+            else
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+
+        }
+    }
+}
---
+++ 05. Small Shop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _05._Small_Shop
+{
+    class Program
+    {
+        static void ChoiseProduct(string product, double[] arr, double volume)
+        {
+            double price = 0;
+            switch (product)
+            {
+                case "coffee":
+                    price = volume * arr[0];
+                    Console.WriteLine(price);
+                    break;
+                case "water":
+                    price = volume * arr[1];
+                    Console.WriteLine(price);
+                    break;
+                case "beer":
+                    price = volume * arr[2];
+                    Console.WriteLine(price);
+                    break;
+                case "sweets":
+                    price = volume * arr[3];
+                    Console.WriteLine(price);
+                    break;
+                case "peanuts":
+                    price = volume * arr[4];
+                    Console.WriteLine(price);
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void Main()
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+
+            double[] sofia = { 0.50, 0.80, 1.2, 1.45, 1.6 };
+            double[] plovdiv = { 0.40, 0.70, 1.15, 1.30, 1.50 };
+            double[] varna = {0.45, 0.70, 1.10, 1.35, 1.55 };
+
+            if (city == "Sofia")
+            {
+                ChoiseProduct(product, sofia, volume);
+            }
+            else if (city == "Plovdiv")
+            {
+                ChoiseProduct(product, plovdiv, volume);
+            }
+            else
+            {
+                ChoiseProduct(product, varna, volume);
+            }
+        }
+    }
+}
