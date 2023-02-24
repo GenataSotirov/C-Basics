79dea891a066fef77eacd2a415eb357c30b84956
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:25:33 +0200

    Add files via upload

---
+++ 04. Walking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    class Program
+    {
+        static void Main()
+        {
+            int stepsGoal = 10000;
+            bool isGoalReached = true;
+            string foot = Console.ReadLine();
+            int actualSteps = 0;    
+
+            while (foot != "Going home")
+            {
+                int steps = int.Parse(foot);
+                actualSteps += steps;
+                if (actualSteps >= stepsGoal)
+                {
+                    break;
+                }
+                foot = Console.ReadLine();
+            }
+            if (foot == "Going home")
+            {
+                foot = Console.ReadLine();
+                int steps = int.Parse(foot);
+                actualSteps += steps;
+                if (actualSteps < stepsGoal)
+                {
+                    isGoalReached = false;
+                }
+            }
+            if (isGoalReached)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{actualSteps - stepsGoal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{stepsGoal - actualSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
---
+++ 05._Coins_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Coins_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double instead = double.Parse(Console.ReadLine());
+            int coins = 0;
+            int result = Convert.ToInt32(instead * 100);
+            int temp = 0;
+            int[] arr = { 200, 100, 50, 20, 10, 5, 2 };
+
+            while (result > 0)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    temp = result / arr[i];
+                    coins += temp;
+                    temp = 0;
+                    result = result % arr[i];
+                }
+                if (result == 1)
+                {
+                    coins++;
+                    result = 0;
+                }
+            }
+           Console.WriteLine(coins);
+        }
+  }
+}
