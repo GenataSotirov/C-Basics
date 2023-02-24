e97d8702daf80fd11b2c50ba6d15667aae45f5e1
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:05:07 +0200

    Add files via upload

---
+++ VacationBooksList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main()
+        {
+            int page = int.Parse(Console.ReadLine());
+            int pagePerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int neededHours = page / pagePerHour;
+            Console.WriteLine(neededHours / days);
+        }
+    }
+}
