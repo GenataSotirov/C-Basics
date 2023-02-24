24a86265e3052b90e820361f90fdab7e9e0b28d6
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:03:49 +0200

    Add files via upload

---
+++ USDtoBGN/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace USDtoBGN
+{
+    class Program
+    {
+        static void Main()
+        {
+            double usd = double.Parse(Console.ReadLine());
+            Console.WriteLine(usd * 1.79549);
+        }
+    }
+}
