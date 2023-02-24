1256eccf5c06408f9459fb81225302c207b7576d
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:24:14 +0200

    Add files via upload

---
+++ 07. Trekking Mania/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    class Program
+    {
+        static void Main()
+        {
+            int groups = int.Parse(Console.ReadLine());
+            int totalCount = 0;
+            double musala = 0;
+            double monblan = 0;
+            double kilimandgaro = 0;
+            double kTwo = 0;
+            double everest = 0;
+
+            for (int i = 0; i < groups; i++)
+            {
+                int people = int.Parse(Console.ReadLine());
+                totalCount+=people;
+
+                if (people <= 5)
+                {
+                    musala += people;
+                }
+                else if (people <= 12)
+                {
+                    monblan += people;
+                }
+                else if (people <= 25)
+                {
+                    kilimandgaro += people;
+                }
+                else if (people <= 40)
+                {
+                    kTwo += people;
+                }
+                else
+                {
+                     everest += people;
+                }
+            }
+            Console.WriteLine($"{((musala/totalCount)*100):f2}%");
+            Console.WriteLine($"{((monblan / totalCount) * 100):f2}%");
+            Console.WriteLine($"{((kilimandgaro / totalCount) * 100):f2}%");
+            Console.WriteLine($"{((kTwo / totalCount) * 100):f2}%");
+            Console.WriteLine($"{((everest / totalCount) * 100):f2}%");
+        }
+    }
+}
---
+++ 08. Tennis Ranklist/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _08._Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main()
+        {
+            int tours = int.Parse(Console.ReadLine());
+            int startPoint = int.Parse(Console.ReadLine());
+            int tourPoint = 0;
+            double countWin = 0;
+
+            for (int i = 0; i < tours; i++)
+            {
+                string tourEtap = Console.ReadLine();
+                if (tourEtap == "W")
+                {
+                    countWin++;
+                    tourPoint += 2000;
+                }
+                else if (tourEtap == "F")
+                {
+                    tourPoint += 1200;
+                }
+                else if (tourEtap == "SF")
+                {
+                    tourPoint += 720;
+                }
+            }
+            double totalPoint = startPoint + tourPoint;
+            double averagePoints = tourPoint / tours;
+            Console.WriteLine($"Final points: {totalPoint}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{((countWin / tours)*100):f2}%");
+        }
+    }
+}
