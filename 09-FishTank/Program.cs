1e2088a0e99854edbdd7d0f6c06959d7dc5ba156
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:01:20 +0200

    Add files via upload

---
+++ 09-FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09_FishTank
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sideA = int.Parse(Console.ReadLine());
+            int sideB = int.Parse(Console.ReadLine());
+            int sideH = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            int volume = sideA * sideB * sideH;
+            double waterVolume = volume - volume * percent / 100;
+
+            Console.WriteLine(waterVolume / 1000);
+        }
+    }
+}
