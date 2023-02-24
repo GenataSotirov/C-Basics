6171f1180e68c30858b63d305d868c1999364473
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:19:51 +0200

    Add files via upload

---
+++ 01. Rectangle of 10 x 10 Stars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Rectangle_of_10_x_10_Stars
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 09. House/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace _09._House
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= n; col++)
+                {
+                    if (row == 1 && n % 2 == 0)
+                    {
+                        if (col <= n/2-1)
+                        {
+                            Console.Write("-");
+                        }
+                        else if (col > n / 2 + 1)
+                        {
+                            Console.Write("-");
+                        }
+                        else
+                        {
+                            Console.Write("*"); 
+                        }
+                    }
+                    if (row == 1 && n % 2 != 0)
+                    {
+                        if (col <= n / 2)
+                        {
+                            Console.Write("-");
+                        }
+                        else if (col > n / 2 + 1)
+                        {
+                            Console.Write("-");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                    }
+                    if (row == 2 && n == 2)
+                    {
+                        Console.Write("|");
+                    }
+                    if (row >=2 && n >=2)
+                    {
+                        if (n%2 == 0)
+                        {
+                            if (row > n/2)
+                            {
+                                if (col == 1 || col == n)
+                                {
+                                    Console.Write("|");
+                                }
+                                else
+                                {
+                                    Console.Write("*");
+                                }
+                            }
+                            else if (col <= n / 2 - row)
+                            {
+                                Console.Write("-");
+                            }
+                            else if (col > n / 2 + row)
+                            {
+                                Console.Write("-");
+                            }
+                            else
+                            {
+                                Console.Write("*");
+                            }
+                        }
+                        else
+                        {
+                            if (row > n/2+1)
+                            {
+                                if (col == 1 || col == n)
+                                {
+                                    Console.Write("|");
+                                }
+                                else
+                                {
+                                    Console.Write("*");
+                                }
+                            }
+                            else if (col <= n / 2 - row+1)
+                            {
+                                Console.Write("-");
+                            }
+                            else if (col > n / 2 + row)
+                            {
+                                Console.Write("-");
+                            }
+                            else
+                            {
+                                Console.Write("*");
+                            }
+                        }
+                        
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
