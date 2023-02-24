5d58059b9ab8c2cec00b10acf8f19ed0135aea47
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:02:06 +0200

    Add files via upload

---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double dividend = double.Parse(Console.ReadLine());
+
+            double dividendPerYear = deposit * (dividend / 100);
+            double total = deposit + (dividendPerYear / 12) * months;
+            Console.WriteLine(total);
+        }
+    }
+}
