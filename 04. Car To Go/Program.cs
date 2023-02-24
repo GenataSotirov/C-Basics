cb363f0f6e6cda72978a3c794715408402cabb6b
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:11:19 +0200

    Add files via upload

---
+++ 04. Car To Go/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _04._Car_To_Go
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string type = string.Empty;
+            string car = string.Empty;
+            double price = 0;
+
+            if (budget <= 100)
+            {
+                type = "Economy class";
+                if (season == "Summer")
+                {
+                    car = "Cabrio";
+                    price = budget * 0.35;
+                }
+                else
+                {
+                    car = "Jeep";
+                    price = budget * 0.65;
+                }
+            }
+            else if (budget <= 500)
+            {
+                type = "Compact class";
+                if (season == "Summer")
+                {
+                    car = "Cabrio";
+                    price = budget * 0.45;
+                }
+                else
+                {
+                    car = "Jeep";
+                    price = budget * 0.8;
+                }
+            }
+            else
+            {
+                type = "Luxury class";
+                car = "Jeep";
+                price = budget * 0.9;
+            }
+            Console.WriteLine(type);
+            Console.WriteLine($"{car} - {price:f2}");
+        }
+    }
+}
---
+++ 07. School Camp/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _07._School_Camp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string season = Console.ReadLine();
+            string groupType = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            int night = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            string sport = string.Empty;
+
+            if (season == "Winter")
+            {
+                if (groupType == "mixed")
+                {
+                    price = count * 10 * night;
+                    sport = "Ski";
+                }
+                else if (groupType == "girls")
+                {
+                    price = count * 9.6 * night;
+                    sport = "Gymnastics";
+                }
+                else 
+                {
+                    price = count * 9.6 * night;
+                    sport = "Judo";
+                }
+            }
+            else if (season == "Summer")
+            {
+                if (groupType == "mixed")
+                {
+                    price = count * 20 * night;
+                    sport = "Swimming";
+                }
+                else if (groupType == "girls")
+                {
+                    price = count * 15 * night;
+                    sport = "Volleyball";
+                }
+                else
+                {
+                    price = count * 15 * night;
+                    sport = "Football";
+                }
+            }
+            else
+            {
+                if (groupType == "mixed")
+                {
+                    price = count * 9.5 * night;
+                    sport = "Cycling";
+                }
+                else if (groupType == "girls")
+                {
+                    price = count * 7.2 * night;
+                    sport = "Athletics";
+                }
+                else
+                {
+                    price = count * 7.2 * night;
+                    sport = "Tennis";
+                }
+            }
+            if (count >= 10 && count < 20)
+            {
+                price = price - price * 0.05;
+            }
+            else if (count < 50 && count >= 20)
+            {
+                price = price - price * 0.15;
+            }
+            else if (count >= 50)
+            {
+                price = price - price * 0.5;
+            }
+            Console.WriteLine($"{sport} {price:f2} lv.");
+        }
+    }
+}
