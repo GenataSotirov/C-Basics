df0b9d53ed06df4caff1113ff1f11bc25a0c7445
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:21:48 +0200

    Add files via upload

---
+++ 02. Half Sum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                sum = sum + temp;
+                if (temp > max)
+                {
+                    max = temp;
+                }
+            }
+            sum = sum - max;
+            if ( sum == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(max - sum)}");
+            }
+        }
+    }
+}
---
+++ 03. Histogram/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _03._Histogram
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double pOne = 0;
+            double pTwo = 0;
+            double pThree = 0;
+            double pFour = 0;
+            double pFive =0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 200)
+                {
+                    pOne++;
+                }
+                else if (num < 400)
+                {
+                    pTwo++;
+                }
+                else if (num < 600)
+                {
+                    pThree++;
+                }
+                else if (num < 800)
+                {
+                    pFour++;
+                }
+                else
+                {
+                    pFive++;
+                }
+            }
+            Console.WriteLine($"{(pOne / n) * 100:f2}%");
+            Console.WriteLine($"{(pTwo / n) * 100:f2}%");
+            Console.WriteLine($"{(pThree / n) * 100:f2}%");
+            Console.WriteLine($"{(pFour / n) * 100:f2}%");
+            Console.WriteLine($"{(pFive / n) * 100:f2}%");
+        }
+    }
+}
