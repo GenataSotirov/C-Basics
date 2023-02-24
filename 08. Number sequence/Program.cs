6093d31ed6d603ba914cd288aec032f4bef584cd
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:29:22 +0200

    Add files via upload

---
+++ 08. Number sequence/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int numMin = int.MaxValue;
+            int numMax = int.MinValue;
+
+            for (int i = 0; i < num; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                int tempOne = temp;
+                int tempTwo = temp;
+                if (tempOne < numMin)
+                {
+                    numMin = temp;
+                }
+                if (tempTwo > numMax)
+                {
+                    numMax = temp;
+                }
+            }
+            Console.WriteLine($"Max number: {numMax}");
+            Console.WriteLine($"Min number: {numMin}");
+        }
+    }
+}
---
+++ 09. Left and Right Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                sumLeft = sumLeft + temp;
+            }
+            for (int i = 0; i < num; i++)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                sumRight = sumRight + temp;
+            }
+            if (sumRight == sumLeft)
+            {
+                Console.WriteLine($"Yes, sum = {sumLeft}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sumLeft - sumRight)}");
+            }
+        }
+    }
+}
