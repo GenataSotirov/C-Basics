bedce4236476f5655ea1c9ffb06755883ea3bf3c
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 14:59:43 +0200

    Add files via upload

---
+++ 07-Food Delivery/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07_Food_Delivery
+{
+    class Program
+    {
+        static void Main()
+        {
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int vegan = int.Parse(Console.ReadLine());
+
+            double foodPrice = chicken * 10.35 + fish * 12.40 + vegan * 8.15;
+            double desert = foodPrice * 20 / 100;
+            double order = foodPrice + desert + 2.50;
+
+            Console.WriteLine(order);
+        }
+    }
+}
