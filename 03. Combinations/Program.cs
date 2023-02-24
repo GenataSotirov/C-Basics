102b9a6f430d61affe578f3f1b5654dcc048c798
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:07:34 +0200

    Add files via upload

---
+++ 03. Combinations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int z = 0; z <= n; z++)
+                    {
+                        if (i + j + z == n)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
