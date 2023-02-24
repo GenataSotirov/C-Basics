dcb43cf8acacad2bcaf64ac012e8774fe48599b7
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 14:58:09 +0200

    Add files via upload

---
+++ 06-Repainting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06_Repainting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            double color = double.Parse(Console.ReadLine());
+            int diluent = int.Parse(Console.ReadLine());
+            int workHours = int.Parse(Console.ReadLine());
+
+            double materials = (nylon + 2) * 1.5 + (color + color / 10) * 14.50 + diluent * 5.00 + 0.40;
+            double hourPrice = materials * 30 / 100;
+            double total = materials + workHours * hourPrice;
+
+            Console.WriteLine(total);
+        }
+    }
+}
