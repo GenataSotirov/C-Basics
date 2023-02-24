86155f51d1674df634f6ab1ad77b0bb1896e0996
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:07:16 +0200

    Add files via upload

---
+++ 01. Sum Seconds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int total = first + second + third;
+
+            int minutes = total / 60;
+            int seconds = total % 60;
+            //string sec = "a";
+
+                if (seconds < 10)
+                {
+                    //sec = "0" + seconds;
+                    Console.WriteLine(minutes + ":0" + seconds);
+                }
+                else
+                {
+                    Console.WriteLine(minutes + ":" + seconds);
+                }
+        }
+    }
+}
---
+++ 02. Bonus Score/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Bonus_Score
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            double bonus = 0;
+
+            if (number <101)
+            {
+                bonus += 5;
+            }
+            else if (number < 1001)
+            {
+                bonus = bonus + number * 0.2;
+            }
+            else
+            {
+                bonus = bonus + number * 0.1;
+            }
+
+            if (number%2 == 0)
+            {
+                bonus++;
+            }
+            else if (number%10 == 5)
+            {
+                bonus += 2;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(number + bonus);
+        }
+    }
+}
