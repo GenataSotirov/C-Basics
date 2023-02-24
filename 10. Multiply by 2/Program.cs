b55bb40417a48d5fd43be1c6d304bedb132f25c5
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:12:27 +0200

    Add files via upload

---
+++ 10. Multiply by 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10._Multiply_by_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            while (num >= 0)
+            {
+                double result = num * 2;
+                Console.WriteLine($"Result: {result:f2}");
+                num = double.Parse(Console.ReadLine());
+            }
+            if (num < 0)
+            {
+                Console.WriteLine("Negative number!");
+            }
+            
+        }
+    }
+}
