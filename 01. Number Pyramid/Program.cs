94118d7da0216f8e55ca81aa5dc98e5c2a48a149
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:03:03 +0200

    Add files via upload

---
+++ 01. Number Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int current = 1;
+
+            for (int row = 1; row <= num; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (current > num)
+                    {
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++; 
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _03._Sum_Prime_Non_Prime
+{
+    class Program
+    {
+        static void Main()
+        {
+            string number = Console.ReadLine();
+            int sumPrime = 0;
+            int sumNonPrime = 0;
+
+            while (number != "stop")
+            {
+                int current = int.Parse(number);
+                bool isPrime = true;
+                if (current < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                }
+                else
+                {
+                    if (current == 1)
+                    {
+                        sumNonPrime++;
+                    }
+                    if (current == 2)
+                    {
+                        isPrime = true;
+                    }
+                    else if (current % 2 == 0)
+                    {
+                        isPrime = false;
+                    }
+                    else
+                    {
+                        for (int i = 3; i < current; i += 2)
+                        {
+                            if (current % i == 0)
+                            {
+                                isPrime = false;
+                                break;
+                            }
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        sumPrime += current;
+                    }
+                    else
+                    {
+                        sumNonPrime += current;
+                    }
+                }
+                number = Console.ReadLine();
+            }
+            Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
+        }
+    }
+}
