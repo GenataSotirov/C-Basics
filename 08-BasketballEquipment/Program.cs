c9c86f63637b4ed574a5e859caae5b2aa3a375bd
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:00:25 +0200

    Add files via upload

---
+++ 08-BasketballEquipment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08_BasketballEquipment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int yearTax = int.Parse(Console.ReadLine());
+
+            double jordan = yearTax - yearTax * 40 / 100;
+            double clotes = jordan - jordan * 20 / 100;
+            double ball = clotes / 4;
+            double accessories = ball / 5;
+
+            double totalTax = yearTax + jordan + clotes + ball + accessories;
+
+            Console.WriteLine(totalTax);
+        }
+    }
+}
