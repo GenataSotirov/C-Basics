42c295f7000a9f9c129ddb98f92c01c3e6aa74b4
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 15:26:08 +0200

    Add files via upload

---
+++ 04-PasswordGuess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04_PasswordGuess
+{
+    class Program
+    {
+        static void Main()
+        {
+            string pass = Console.ReadLine();
+            string word = "s3cr3t!P@ssw0rd";
+
+            if (pass == word)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
