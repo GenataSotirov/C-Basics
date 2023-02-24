23b903f959cb85b578318d1102fc35ca864f3d77
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:21:06 +0200

    Add files via upload

---
+++ 13. Prime Pairs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _13._Prime_Pairs
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstStart = int.Parse(Console.ReadLine());
+            int secondStart = int.Parse(Console.ReadLine());
+            int firstDifference = int.Parse(Console.ReadLine());
+            int secondDifference = int.Parse(Console.ReadLine());
+
+            int firstEnd = firstStart + firstDifference;
+            int secondEnd = secondStart + secondDifference;
+            bool isPrime = false;
+
+            for (int i = firstStart; i <= firstEnd; i++)
+            {            
+                if (i%2 != 0)
+                {
+                    for (int j = 3; j < i; j += 2)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                        else
+                        {
+                            isPrime = true;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        int num1 = i;
+                        int num2 = 0;
+
+                        for (int k = secondStart; k <= secondEnd; k++)
+                        {
+                            bool secondPrime = false;
+                            if (k%2 !=0)
+                            {
+                                for (int l = 3; l < k; l+=2)
+                                {
+                                    if (k%l==0)
+                                    {
+                                        secondPrime= false;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        secondPrime = true;
+                                    }
+                                }
+                                if (secondPrime)
+                                {
+                                      num2 = k;
+                                    Console.WriteLine($"{num1}{num2}");
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 14. Password Generator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _14._Password_Generator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    
+                    for (int k = 97; k < 97 + l; k++)
+                    {
+                        for (int h = 97; h < 97 + l; h++)
+                        {
+                            for (int f = 2; f <= n; f++)
+                            {
+                                char first = (char)k;
+                                char second = (char)h;
+                                if (f > i && f > j)
+                                {
+                                    Console.Write($"{i}{j}{first}{second}{f} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
