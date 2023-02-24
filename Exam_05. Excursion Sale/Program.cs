780ea56fbe9cf8b5f8c569e758435515825a6c95
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:49:40 +0200

    Add files via upload

---
+++ Exam_05. Excursion Sale/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05._Excursion_Sale
+{
+    class Program
+    {
+        static void Main()
+        {
+            int seaTrip = int.Parse(Console.ReadLine());
+            int mountainTrip = int.Parse(Console.ReadLine());
+
+            int count = seaTrip + mountainTrip;
+            string input = Console.ReadLine();
+            int totalSum = 0;
+
+            while (input != "Stop" && count > 0)
+            {
+                if (input == "sea" && seaTrip > 0)
+                {
+                    if (seaTrip > 0)
+                    {
+                        seaTrip--;
+                        totalSum += 680;
+                    }
+                }
+                else if (input == "mountain" && mountainTrip > 0)
+                {
+                    if (mountainTrip > 0)
+                    {
+                        mountainTrip--;
+                        totalSum += 499;
+                    }
+                }
+                count = seaTrip + mountainTrip;
+                if (count > 0)
+                {
+                    input = Console.ReadLine();
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Good job! Everything is sold.");
+            }
+            Console.WriteLine($"Profit: {totalSum} leva.");
+        }
+    }
+}
