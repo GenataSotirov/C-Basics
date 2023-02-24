9753b1f08cdf438377c27f94d1a7a30900f620fe
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:27:27 +0200

    Add files via upload

---
+++ 06. Cake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            string comand = Console.ReadLine();
+
+            int area = length * width;
+            int pieces = 0;
+
+            while (comand != "STOP")
+            {
+                int temp = int.Parse(comand);
+                pieces += temp;
+                if (pieces >= area)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(area - pieces)} pieces more.");
+                    break;
+                }
+                else
+                {
+                    temp = 0;
+                    comand = Console.ReadLine();
+                }
+            }
+            if (comand == "STOP")
+            {
+                Console.WriteLine($"{Math.Abs(area - pieces)} pieces are left.");
+            }
+        }
+    }
+}
---
+++ 07. Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    class Program
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int volume = length * width * height;
+            string comand = Console.ReadLine();
+            int boxes = 0;
+
+            while (comand != "Done")
+            {
+                int temp = int.Parse(comand);
+                boxes += temp;
+                if (boxes >= volume)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(boxes - volume)} Cubic meters more.");
+                    break;
+                }
+                else
+                {
+                    comand = Console.ReadLine();
+                }
+                
+            }
+            if (comand == "Done")
+            {
+                Console.WriteLine($"{Math.Abs(boxes - volume)} Cubic meters left.");
+            }
+        }
+    }
+}
