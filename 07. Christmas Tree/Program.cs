72854c95064040cef2bf9fd7379c16286ed477f0
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:16:50 +0200

    Add files via upload

---
+++ 07. Christmas Tree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07._Christmas_Tree
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int k = (n - i -1) ; k >= 0; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" | ");
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
