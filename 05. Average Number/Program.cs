5ccebd54e7073bdcfd804da48ea747306a64861b
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:38:48 +0200

    Add files via upload

---
+++ 05. Average Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                count++;
+                sum += num;
+            }
+            Console.WriteLine($"{(double)sum/count:f2}");
+        }
+    }
+}
