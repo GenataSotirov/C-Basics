3d81a08ad1f560cc45e1cc65f446c7bf8e484d21
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:36:42 +0200

    Add files via upload

---
+++ 01. Dishwasher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _01._Dishwasher
+{
+    class Program
+    {
+        static void Main()
+        {
+            int detergent = int.Parse(Console.ReadLine()) * 750;
+
+            string input = Console.ReadLine();
+            bool notEnough = true;
+            int countDishes = 0;
+            int countPots = 0;
+            int count = 1;
+
+            while (input != "End")
+            {
+                int dishes = int.Parse(input);
+                
+                if (count%3==0)
+                {
+                    detergent -= dishes * 15; 
+                    countPots += dishes;
+                    count++;
+                }
+                else
+                {
+                    countDishes += dishes;
+                    detergent -= dishes * 5;
+                    count++;
+                }
+                if (detergent<0)
+                {
+                    notEnough = false;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (notEnough)
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{countDishes} dishes and {countPots} pots were washed.");
+                Console.WriteLine($"Leftover detergent {detergent} ml.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough detergent, {Math.Abs(detergent)} ml. more necessary!");
+            }
+        }
+    }
+}
