61308632cda6efe833afbe6f2611b354fed902cf
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 14:56:42 +0200

    Add files via upload

---
+++ 05-SuppliesForSchool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05_SuppliesForSchool
+{
+    class Program
+    {
+        static void Main()
+        {
+            int pencil = int.Parse(Console.ReadLine());
+            int marker = int.Parse(Console.ReadLine());
+            int gel = int.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double sum = pencil * 5.80 + marker * 7.20 + gel * 1.20;
+            double final = sum - sum * discount / 100;
+
+            Console.WriteLine(final);
+        }
+    }
+}
