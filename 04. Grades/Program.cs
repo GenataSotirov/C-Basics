a10df73e20a50c5d390dcf7abcb0a7845b6b64ef
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 16:33:17 +0200

    Add files via upload

---
+++ 04. Grades/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04._Grades
+{
+    class Program
+    {
+        static void Main()
+        {
+            int students = int.Parse(Console.ReadLine());
+
+            int countTop = 0;
+            int countFour = 0;
+            int countThree = 0;
+            int countFail = 0;
+            double averageGrade = 0;
+
+            for (int i = 1; i <= students; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                averageGrade += grade;
+
+                if (grade>=5)
+                {
+                    countTop++;
+                }
+                else if (grade >= 4)
+                {
+                    countFour++;
+                }
+                else if (grade >= 3)
+                {
+                    countThree++;
+                }
+                else
+                {
+                    countFail++;
+                }
+            }
+            Console.WriteLine($"Top students: {(double)countTop/students*100:f2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {(double)countFour/students*100:f2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {(double)countThree/students*100:f2}%");
+            Console.WriteLine($"Fail: {(double)countFail/students*100:f2}%");
+            Console.WriteLine($"Average: {averageGrade/students:f2}");
+        }
+    }
+}
---
+++ 06. Bills/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Bills
+{
+    class Program
+    {
+        static void Main()
+        {
+            int months = int.Parse(Console.ReadLine());
+            double current = 0;
+            double electricity = 0;
+            double water = 0;
+            double internet = 0;
+            double other = 0;
+
+            for (int i = 1; i <= months; i++)
+            {
+                current = double.Parse(Console.ReadLine());
+                electricity += current;
+                water += 20;
+                internet += 15;
+                other += (current + 20 + 15) * 1.2;
+            }
+            Console.WriteLine($"Electricity: {electricity:f2} lv");
+            Console.WriteLine($"Water: {water:f2} lv");
+            Console.WriteLine($"Internet: {internet:f2} lv");
+            Console.WriteLine($"Other: {other:f2} lv");
+            Console.WriteLine($"Average: {(electricity+water+internet+other)/months:f2} lv");
+        }
+    }
+}
