3ec4fdd8e1208e9ac085e2f2de8a010fb15c8dc9
GenataSotirov <126165451+GenataSotirov@users.noreply.github.com>
2023-02-24 19:31:04 +0200

    Add files via upload

---
+++ 01. Read Text/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Read_Text
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool stop = true;
+
+            while (stop)
+            {
+                string name = Console.ReadLine();
+                if (name != "Stop")
+                {
+                    Console.WriteLine(name);
+                }
+                else
+                {
+                    stop = false;
+                }
+            }
+        }
+    }
+}
---
+++ 03. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < number)
+            {
+                int temp = int.Parse(Console.ReadLine());
+                sum += temp;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
